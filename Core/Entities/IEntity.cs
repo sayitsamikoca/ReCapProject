b7dda3f39266f4d56b48f442a36d4ce0bce3777d
Sayit Sami Koca <sayitsamikoca0@gmail.com>
2023-02-25 18:23:00 +0300

    code refactoring

---
+++ Core/Entities/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    // IEntity implmente eden sınıf bir veritabanı tablosudur.
+    public interface IEntity
+    {
+    }
+}
