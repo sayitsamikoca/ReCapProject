57e6885f832f1b66e88fd5e3c270211b9d5c572e
Sayit Sami Koca <sayitsamikoca0@gmail.com>
2023-03-08 16:21:49 +0300

    Constants yapısı eklendi ve standart mesajlar yazıldı.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public class Messages
+    {
+        public static string CarAdded = "Araba Eklendi.";
+        public static string CarNameInvalid = "Araba ismi geçersiz.";
+        public static string CarDeleted = "Araba Silindi.";
+        public static string CarNotFound = "Sistemde kayıtlı araba bulunamadı";
+        public static string CarUpdated = "Araba Güncellendi";
+
+
+    }
+}
