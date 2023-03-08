fd9ffa08223f161acc02be5c3d8cf2dec7d0623e
Sayit Sami Koca <sayitsamikoca0@gmail.com>
2023-03-08 17:55:21 +0300

    Maintenance Time mesajı eklendi

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,7 +11,9 @@ namespace Business.Constants
         public static string CarDeleted = "Araba Silindi.";
         public static string CarNotFound = "Sistemde kayıtlı araba bulunamadı";
         public static string CarUpdated = "Araba Güncellendi";
-
+        public static string CarListed = "Araba Güncellendi";
+        public static string CarDetailsListed = "Araba Detayları Listelendi";
+        public static string MaintenanceTime = "Sistem bakımda !";
 
     }
 }
